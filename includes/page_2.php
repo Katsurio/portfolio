c4e4f193f00b7b378b87f39446050b7cca55c340
John (Jef) Fisher <Fishe545@gmail.com>
2017-05-23 23:30:56 -0700

    Includes are finished. Need to customize and add G Maps

--- includes/page_2.php
+++ includes/page_2.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Katsurio
- * Date: 5/22/17
- * Time: 1:29 PM
- */
+<section id="page2">
+    <div class="content">
+        <div class="container clearfix">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="heading">About me</h2>
+                    <p class="lead">This can be an about section or anything else ;)</p>
+                    <p>Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation. </p>
+                    <p>Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery. </p>
+                </div>
+                <div class="col-md-5 col-md-offset-1">
+                    <p><img src="portfolio_1_screen_BASE/img/about.jpg" alt="" class="img-responsive img-circle"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- includes/page_6.php
+++ includes/page_6.php
@@ -1,7 +1,58 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Katsurio
- * Date: 5/22/17
- * Time: 1:29 PM
- */
+<section id="page6">
+    <div class="content">
+        <div class="container clearfix">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="heading">Contact</h2>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <form id="contact-form" method="post" action="contact.php" class="contact-form">
+                                <div class="controls">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label for="name">Your firstname *</label>
+                                                <input type="text" name="name" placeholder="Enter your firstname" required="required" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label for="surname">Your lastname *</label>
+                                                <input type="text" name="surname" placeholder="Enter your  lastname" required="required" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="surname">Your email *</label>
+                                        <input type="email" name="email" placeholder="Enter your  email" required="required" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="surname">Your message for us *</label>
+                                        <textarea rows="4" name="message" placeholder="Enter your message" required="required" class="form-control"></textarea>
+                                    </div>
+                                    <div class="text-center">
+                                        <input type="submit" name="name" value="Send message" class="btn btn-primary btn-block">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-6">
+                            <p>Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating. Court front maids forty if aware their at. Chicken use are pressed removed. </p>
+                            <p>Able an hope of body. Any nay shyness article matters own removal nothing his forming. Gay own additions education satisfied the perpetual. If he cause manor happy. Without farther she exposed saw man led. Along on happy could cease green oh. </p>
+                            <p class="social"><a href="#" title="" class="facebook"><i class="fa fa-facebook"></i></a><a href="#" title="" class="twitter"><i class="fa fa-twitter"></i></a><a href="#" title="" class="gplus"><i class="fa fa-google-plus"></i></a><a href="#" title="" class="instagram"><i class="fa fa-instagram"></i></a><a href="#" title="" class="email"><i class="fa fa-envelope"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="row copyright">
+                        <div class="col-md-6">
+                            <p>&copy;2015 Your name/company goes here</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p class="credit">Code by <a href="https://bootstrapious.com/portfolio-themes">Bootstrapious</a></p>
+                            <!-- Not removing these links is part of the license conditions of the template. Thanks for understanding :) If you want to use the template without the attribution links, you can do so after supporting further themes development at https://bootstrapious.com/donate  -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
